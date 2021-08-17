cf644e23131812f7b90a7d3986a4e0165e44b8f3
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-08-17 14:22:21 +0200

    landing page updated

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2021_08_16_152718) do
 
-ActiveRecord::Schema.define(version: 2021_08_16_151517) do
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "brains", force: :cascade do |t|
     t.string "name"
@@ -39,8 +35,6 @@ ActiveRecord::Schema.define(version: 2021_08_16_151517) do
     t.index ["brain_id"], name: "index_reviews_on_brain_id"
   end
 
-  add_foreign_key "reviews", "brains"
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,5 +47,5 @@ ActiveRecord::Schema.define(version: 2021_08_16_151517) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
+  add_foreign_key "reviews", "brains"
 end
