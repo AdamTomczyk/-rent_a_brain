f39bb08cb18665e064b801aa7354580072e9edf5
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-08-18 11:11:22 +0200

    Backup

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_121940) do
+
+ActiveRecord::Schema.define(version: 2021_08_17_145727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "brain_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.boolean "availability"
+    t.integer "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["brain_id"], name: "index_bookings_on_brain_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "brains", force: :cascade do |t|
     t.string "name"
@@ -49,6 +63,10 @@ ActiveRecord::Schema.define(version: 2021_08_17_121940) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+
+  add_foreign_key "bookings", "brains"
+  add_foreign_key "bookings", "users"
 
   add_foreign_key "brains", "users"
   add_foreign_key "reviews", "brains"
