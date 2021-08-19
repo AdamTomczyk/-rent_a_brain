c6953dbc340a947be0af093183fd5db75027a75b
Aija Mezapuke <aija.mezapuke@gmail.com>
2021-08-19 11:51:04 +0200

    migration removed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_103340) do
+ActiveRecord::Schema.define(version: 2021_08_19_094545) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
